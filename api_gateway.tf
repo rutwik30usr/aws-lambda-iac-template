78adb72dae9a2b3bc36353b74b4f3bcf1a7879df
rutwik <rutwiksatpute3034@gmail.com>
2026-02-13 18:16:28 +0530

    adding commit

---
+++ api_gateway.tf
@@ -0,0 +1,30 @@
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "lambda-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_alias.my_lambda_alias.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_alias.my_lambda_alias.function_name
+  principal     = "apigateway.amazonaws.com"
+}
